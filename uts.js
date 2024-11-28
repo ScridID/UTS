661806077a668dd0c3eb956e92aec4a7cf0ae7fa
HumanError <120297349+ScridID@users.noreply.github.com>
2024-11-28 19:49:36 +0700

    Update uts.js

--- uts.js
+++ uts.js
@@ -1,12 +1,12 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#tanaman-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
 // Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+const hamburger = document.querySelector("#tanaman-menu");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
